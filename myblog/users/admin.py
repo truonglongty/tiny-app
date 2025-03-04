f3f011b48dc4c7f488e3b3b6793672615ad659b1
truonglongty <137083025+truonglongty@users.noreply.github.com>
2025-03-04 17:58:39 +0700

    add features: block and reset password

--- myblog/users/admin.py
+++ myblog/users/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
-from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import AdminPasswordChangeForm
+from django.contrib import messages
 
-# Register your models here.
-admin.site.register(Profile)
+class UserAdmin(BaseUserAdmin):
+    actions = ['block_user', 'unblock_user', 'reset_password']
+
+    def block_user(self, request, queryset):
+        queryset.update(is_active=False)
+        messages.success(request, "Selected users have been blocked.")
+
+    block_user.short_description = "Block selected users"
+
+    def unblock_user(self, request, queryset):
+        queryset.update(is_active=True)
+        messages.success(request, "Selected users have been unblocked.")
+
+    unblock_user.short_description = "Unblock selected users"
+
+    def reset_password(self, request, queryset):
+        for user in queryset:
+            form = AdminPasswordChangeForm(user, {'password1': 'new_password', 'password2': 'new_password'})
+            if form.is_valid():
+                form.save()
+        messages.success(request, "Passwords for selected users have been reset to 'new_password'.")
+
+    reset_password.short_description = "Reset password for selected users"
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- myblog/users/views.py
+++ myblog/users/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.shortcuts import redirect
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import authenticate, login
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -12,14 +12,32 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}! You are now able to log in')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('profile')
+            else:
+                messages.error(request, 'Your account has been blocked.')
+        else:
+            messages.error(request, 'Please enter a correct username and password. Note that both fields may be case-sensitive.')
+    return render(request, 'users/login.html')
 
 @login_required
 def profile(request):
+    if request.user.is_active == False:
+        messages.error(request, 'Your account has been blocked.')
+        auth_logout(request)
+        return redirect('login')
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
